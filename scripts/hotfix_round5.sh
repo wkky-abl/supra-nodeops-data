e0444ca42650411ddd6778c3d3b97803d64777f9
soham <s.jadiya@supraoracles.com>
2024-08-16 20:11:37 +0530

    updating hotfix_round5 script

--- scripts/hotfix_round5.sh
+++ scripts/hotfix_round5.sh
@@ -15,24 +15,24 @@ container_id="supra_${ip_address}"
 echo "Stopping supra container $container_id if running"
 if docker ps | grep "$container_id" > /dev/null; then
   if ! docker stop "$container_id"; then
-      echo "Failed to stop supra container. Exiting..."
+      echo "Failed to stop supra container."
   fi
   echo "Supra container stopped"
 else
-  echo "Supra container is not running. Exiting..."
+  echo "Supra container is not running."
 fi
 
 # Remove the Docker container
 echo "Removing supra container"
 if ! docker rm "$container_id"; then
-    echo "Failed to remove supra container. Exiting..."
+    echo "Failed to remove supra container."
 fi
 echo "Supra container removed"
 
 # Remove the old Docker image
 echo "Deleting old docker image"
 if ! docker rmi asia-docker.pkg.dev/supra-devnet-misc/smr-moonshot-devnet/validator-node:v5.0.0; then
-    echo "Failed to delete old Docker image. Exiting..."
+    echo "Failed to delete old Docker image."
 fi
 echo "Deleted the old Docker image"
 
@@ -46,8 +46,7 @@ if ! docker run --name "$container_id" \
     -e="SUPRA_MAX_UNCOMPRESSED_LOGS=5" \
     -e="SUPRA_MAX_LOG_FILES=20" \
     --net=host -itd asia-docker.pkg.dev/supra-devnet-misc/smr-moonshot-devnet/validator-node:v5.0.1; then
-    echo "Failed to run new Docker image. Exiting..."
+    echo "Failed to run new Docker image."
     exit 1
 fi
 echo "New Docker image created"
-
