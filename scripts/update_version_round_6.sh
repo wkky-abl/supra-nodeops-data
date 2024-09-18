1831f6d1f1dabf751e27f6e9348a57de20a42989
soham <s.jadiya@supraoracles.com>
2024-09-18 23:46:40 +0530

    updating the fix

--- scripts/update_version_round_6.sh
+++ scripts/update_version_round_6.sh
@@ -94,7 +94,7 @@ echo "Running new docker image"
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
-if !     docker run --name "supra_$ip" \
+if !     docker run --name "supra_$ip_address" \
         -v $SCRIPT_EXECUTION_LOCATION:/supra/configs \
         --user "$USER_ID:$GROUP_ID" \
         -e "SUPRA_HOME=/supra/configs" \
