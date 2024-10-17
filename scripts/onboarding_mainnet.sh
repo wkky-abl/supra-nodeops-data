9e28d0786c52f861a2174e9fcc2218505f45d0f0
soham <s.jadiya@supraoracles.com>
2024-10-18 05:05:15 +0530

    reverting the fix

--- scripts/onboarding_mainnet.sh
+++ scripts/onboarding_mainnet.sh
@@ -11,9 +11,9 @@ GRAFANA="https://raw.githubusercontent.com/Entropy-Foundation/supra-node-monitor
 GRAFANA_CENTOS="https://raw.githubusercontent.com/Entropy-Foundation/supra-node-monitoring-tool/master/nodeops-monitoring-telegraf-centos.sh"
 
 create_folder_and_files() {
-    touch $CONFIG_FILE
-    if [ ! -d "$SCRIPT_EXECUTION_LOCATION" ]; then
-        mkdir $SCRIPT_EXECUTION_LOCATION
+    touch operator_config_mainnet.toml
+    if [ ! -d "supra_configs_mainnet" ]; then
+        mkdir supra_configs_mainnet
     else
         echo ""
     fi
@@ -52,7 +52,7 @@ check_prerequisites() {
     if ! command -v docker &>/dev/null; then
         echo "Docker is not installed. Please install Docker before proceeding."
         echo "Terminating Script"
-        echo ""
+        echo " "
         exit 1
     fi 
 
@@ -72,28 +72,25 @@ check_prerequisites() {
     # Check if toml-cli is installed
     if ! command -v toml &> /dev/null; then
         echo "toml-cli could not be found. Please install it to proceed."
-        echo "Command : cargo install toml-cli"
+        echo "command : cargo install toml-cli"
         exit 1
     fi 
 
     # Check if sha256sum is installed
     if ! command -v sha256sum &> /dev/null; then
         echo "sha256sum is not installed."
-        echo "Command : sudo apt install coreutils"
         exit 1
     fi
 
     # Check if openssl is installed
     if ! command -v openssl &> /dev/null; then
         echo "openssl is not installed."
-        echo "Command : sudo apt install openssl"
         exit 1
     fi
 
     # Check if zip is installed
     if ! command -v zip &> /dev/null; then
         echo "zip is not installed, please install zip manually."
-        echo "Command : sudo apt install zip"
         exit 1
     fi
 
@@ -1183,8 +1180,7 @@ while true; do
         2)
 
             IP_ADDRESS=$(extract_ip "operator_config_mainnet.toml")
-            dns_name=$(parse_toml "address.Dns.name" "$SCRIPT_EXECUTION_LOCATION/validator_public_identity.toml")
-            FILE_NAME="${dns_name}_genesis_signature.sig"
+            FILE_NAME="$IP_ADDRESS:28000_genesis_signature.sig"
             CONFIG_FILE="$BASE_PATH/operator_config_mainnet.toml"
             enc_password=$(grep '^password' "$CONFIG_FILE" | awk -F' = ' '{print $2}' | tr -d '"')
             decoded_password=$(echo "$enc_password" | openssl base64 -d -A)
@@ -1370,7 +1366,7 @@ while true; do
             exit 0
             ;;
         *)
-            echo "Invalid choice. Please enter a number between 1 and 7."
+            echo "Invalid choice. Please enter a number between 1 and 5."
             ;;
 
     esac
