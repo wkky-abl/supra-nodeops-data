15fc835d4aa03357ab76137f356eb06d0e354197
khaders-supraoracles <127101735+khaders-supraoracles@users.noreply.github.com>
2024-11-11 19:13:58 +0530

    Update update_version_round_6_4.sh
    Add break to the rclone

--- scripts/update_version_round_6_4.sh
+++ scripts/update_version_round_6_4.sh
@@ -84,6 +84,7 @@ while [ $retry_count -lt $MAX_RETRIES ]; do
     # Check if the command was successful
     if [ $? -eq 0 ]; then
         echo "rclone command succeeded."
+        break
     else
         # Command failed
         echo "rclone command failed. Attempt $((retry_count + 1))/$MAX_RETRIES."
